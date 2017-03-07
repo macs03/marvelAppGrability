cf9443265919a306e081141f7fadfcc1184ef66c
Miguel Cardenas <miguelcarde.3@gmail.com>
2017-03-07 12:18:06 -0500

    all structure of commons components

--- src/app/commons/index.js
+++ src/app/commons/index.js
@@ -3,9 +3,13 @@ import angular from 'angular';
 import {
     footerApp
 } from './footer';
+import {
+    headerApp
+} from './header';
 
 export const commonsModule = 'commons';
 
 angular
     .module(commonsModule, [])
-    .component('footerApp', footerApp);
+    .component('footerApp', footerApp)
+    .component('headerApp', headerApp);
